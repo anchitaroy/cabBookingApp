5994e6fee164fdaccfe76760c1d6e2dfcd78a72c
Tester1 <Tester1@gmail.com>
2019-11-05 14:12:03 +0530

    Test Cases added

--- src/main/java/com/cg/cbs/pf/dao/CustomerWalletDao.java
+++ src/main/java/com/cg/cbs/pf/dao/CustomerWalletDao.java
@@ -1,10 +1,10 @@
 package com.cg.cbs.pf.dao;
 
-import com.cg.cbs.pf.dto.Booking;
 import com.cg.cbs.pf.dto.Customer;
 import com.cg.cbs.pf.dto.TripDetails;
 
 public interface CustomerWalletDao {
 
-	Customer updateCustWallet(TripDetails tripDetails);
+	Customer updateCustWallet(double wallet, double finalFare);
+	
 }
--- src/test/java/com/cg/cbs/pf/service/PaymentFeedbackServiceImplTest.java
+++ src/test/java/com/cg/cbs/pf/service/PaymentFeedbackServiceImplTest.java
@@ -2,33 +2,59 @@ package com.cg.cbs.pf.service;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.cg.cbs.pf.dto.TripDetails;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PaymentFeedbackServiceImplTest {
+	
+	@Autowired
+	private PaymentFeedbackService service;
 
 	@Test
 	public void testAddPayment() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testUpdateBooking() {
-		fail("Not yet implemented");
+		TripDetails trip = new TripDetails();
+		trip.setBookingId(1);
+		trip.setCustomerId(11);
+		trip.setDriverId(111);
+		trip.setTripId(7);
+		trip.setFinalFare(350.0);
+		trip.setIssues("No Issues");
+		trip.setWallet(675.0);
+		List<TripDetails> tr = new ArrayList<TripDetails>();
+		tr.add(trip);
+		System.out.println(service.addPayment(trip));
 	}
 
 	@Test
 	public void testUpdateReport() {
-		fail("Not yet implemented");
+		TripDetails trip = new TripDetails();
+		trip.setBookingId(1);
+		trip.setCustomerId(11);
+		trip.setDriverId(111);
+		trip.setTripId(7);
+		trip.setFinalFare(350.0);
+		trip.setIssues("No Issues");
+		trip.setWallet(675.0);
+		List<TripDetails> tr = new ArrayList<TripDetails>();
+		tr.add(trip);
+		System.out.println(service.updateReport(trip));
 	}
 
 	@Test
 	public void testUpdateCustomer() {
-		fail("Not yet implemented");
+		double finalFare = 350.0;
+		double wallet = 675.0;
+		System.out.println(service.updateCustomer(wallet, finalFare));
 	}
 
 }
